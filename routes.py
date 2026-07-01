a4e1b96dd9732a8d86048768bc4469d496a90ffc
laondol <laon.cafe@gmail.com>
2026-07-01 19:31:20 +0900

    관리자관리: 양평AI 가르치기 체크박스 추가

--- routes.py
+++ routes.py
@@ -991,7 +991,7 @@ def register_routes(app):
             },
             {
                 'title': '관리',
-                'pages': {'admin_proposals':'누구의꿈(관리)', 'admin_users':'회원관리', 'admin_news':'소식(관리)', 'admin_share':'공유(관리)', 'admin_stores':'동네가게(관리)', 'admin_alerts':'알림(관리)'}
+                'pages': {'admin_proposals':'누구의꿈(관리)', 'admin_users':'회원관리', 'admin_news':'소식(관리)', 'admin_share':'공유(관리)', 'admin_stores':'동네가게(관리)', 'admin_alerts':'알림(관리)', 'admin_ai_train':'양평AI 가르치기'}
             },
             {
                 'title': '기타',
